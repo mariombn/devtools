866924a3f6dcc3083131676f6803fb84c29ce6a4
Mario de Moraes <mariombn@gmail.com>
2026-02-01 21:34:19 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+		port: 3000,
+		host: true,    // This enables listening on all network interfaces
+	},
+	server: {        // Also add this for development server
+		host: true,    // This enables listening on all network interfaces
+		port: 3000
+	}
 })
